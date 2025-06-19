fb8e26f6e026caf2a3af0777b6047e90eb8783cd
Cloudyky <ikhwanch131@gmail.com>
2025-06-19 17:58:17 +0800

    update

--- dealer.js
+++ dealer.js
@@ -1,11 +1,15 @@
 var brandlist = new Array("Porsche","Volkswagen","Audi","BMW");
+
 function newClient(){
 	var preference = Math.floor((Math.random()*4));
 	var time = Math.floor((Math.random()*10000)+1);
 	var client = Math.floor((Math.random()*10)+1);
+
 	$("#clients_queue").append('<div class="client client_'+client+'"><span class="preference">Client for '+brandlist[preference]+'</span></div>');
+	
 	setTimeout(function(){newClient();},time);
 }
+
 $("document").ready(function(e) {
 	newClient();
 });
