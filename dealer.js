14d4dd6c85857cb0fb5e0125af23fdeacd719ced
Cloudyky <ikhwanch131@gmail.com>
2025-06-20 10:22:10 +0800

    better append

--- dealer.js
+++ dealer.js
@@ -5,11 +5,26 @@ function newClient(){
 	var time = Math.floor((Math.random()*10000)+1);
 	var client = Math.floor((Math.random()*10)+1);
 
-	$("#clients_queue").append('<div class="client client_'+client+'"><span class="preference">Client for '+brandlist[preference]+'</span></div>');
+	// $("#clients_queue").append('<div class="client client_'+client+' draggable"><span class="preference">Client for '+brandlist[preference]+'</span></div>');
+
+	const $client = $('<div>', {
+						class: `client client_${client} draggable`
+					}).append(
+						$('<span>', {
+							class: 'preference',
+							text: `Client for ${brandlist[preference]}`
+						})
+					);
+
+	$("#clients_queue").append($client);
 	
 	setTimeout(function(){newClient();},time);
 }
 
 $("document").ready(function(e) {
+	let clientServed, carsSold, amount, clientInQueue = 0;
+
+	$("#clients_served").html(clientServed);
+
 	newClient();
 });
