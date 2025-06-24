01534fcb47aa0cc690c2ded16f2c86a7493c9b82
Cloudyky <ikhwanch131@gmail.com>
2025-06-24 15:29:03 +0800

    dah siap but ade bug

--- dealer.js
+++ dealer.js
@@ -1,38 +1,35 @@
-var brandlist = new Array("Porsche","Volkswagen","Audi","BMW");
+var brandlist = ["Porsche", "Volkswagen", "Audi", "BMW"];
 let brands = {
-		Porsche: { count: 4, price: 650000 },
-		Volkswagen: { count: 6, price: 180000 },
-		Audi: { count: 5, price: 300000 },
-		BMW: { count: 3, price: 250000 }
-	};
+	Porsche: { count: 4, price: 650000 },
+	Volkswagen: { count: 6, price: 180000 },
+	Audi: { count: 5, price: 300000 },
+	BMW: { count: 3, price: 250000 }
+};
 
-function newClient(){
-	var preference = Math.floor((Math.random()*4));
-	var time = Math.floor((Math.random()*10000)+1);
-	var client = Math.floor((Math.random()*10)+1);
-
-	// $("#clients_queue").append('<div class="client client_'+client+' draggable"><span class="preference">Client for '+brandlist[preference]+'</span></div>');
+function newClient() {
+	var preference = Math.floor(Math.random() * 4);
+	var time = Math.floor(Math.random() * 10000) + 1;
+	var client = Math.floor(Math.random() * 10) + 1;
 
 	const $client = $('<div>', {
-						class: `client client_${client} draggable`,
-						'data-client-id': client,
-						'data-preference': brandlist[preference],
-					}).append(
-						$('<span>', {
-							class: 'preference',
-							text: `Client for ${brandlist[preference]}`
-						})
-					);
+		class: `client client_${client} draggable`,
+		'data-client-id': client,
+		'data-preference': brandlist[preference]
+	}).append(
+		$('<span>', {
+			class: 'preference',
+			text: `Client for ${brandlist[preference]}`
+		})
+	);
 
 	if ($("#clients_queue").children().length < 10) {
 		$("#clients_queue").append($client);
 		$("#client_in_queue").html($("#clients_queue").children().length);
 
-		
 		$(".draggable").draggable({
 			revert: "invalid",
 			containment: "body",
-			helper: function() {
+			helper: function () {
 				const $clone = $(this).clone();
 				$clone.css({
 					width: "100px",
@@ -49,54 +46,44 @@ function newClient(){
 
 		updateDraggableQueue();
 	}
-	
-	setTimeout(function(){newClient();},time);
+
+	setTimeout(() => newClient(), time);
 }
 
 function updateDraggableQueue() {
-	// Disable semua dulu
 	$("#clients_queue .client").draggable("disable");
-
-	// Aktifkan yang pertama sahaja
 	$("#clients_queue .client:first").draggable("enable");
 }
 
-function exit(){
+function exit() {
 	$("#exit").droppable({
 		accept: ".client",
-		over: function(event, ui) {
+		over: function () {
 			$(this).addClass("highlight-dropzone");
 		},
-		out: function(event, ui) {
+		out: function () {
 			$(this).removeClass("highlight-dropzone");
 		},
-		drop: function(event, ui) {
-			$(this).removeClass("highlight-dropzone"); 
+		drop: function (event, ui) {
+			$(this).removeClass("highlight-dropzone");
 			const clientId = ui.draggable.data("client-id");
 
-			// Cari dalam queue
 			const $originalClient = $(`#clients_queue .client[data-client-id='${clientId}']`);
-
 			if ($originalClient.length) {
-				// Kalau client masih dalam queue, buang dari queue
 				$originalClient.remove();
 			} else {
-				// Kalau client dari dalam kereta
 				const $parentDropzone = ui.draggable.closest(".client-drop");
-				
-				// Kosongkan dropzone dan tambah balik teks asal
-				$parentDropzone.empty().html("Drop client here");
+				$parentDropzone.empty().html("Drop client here").removeClass("highlight-dropzone");
 			}
 
-			// Update bilangan client dalam queue
+			let clientsServed = parseInt($("#clients_served").text()) || 0;
+			clientsServed++;
+			$("#clients_served").text(clientsServed);
+
 			$("#client_in_queue").html($("#clients_queue").children().length);
-
-			// Aktifkan client baru dalam queue
 			updateDraggableQueue();
-
-			console.log("Client exited.");
 		}
-	})
+	});
 }
 
 function generateCars() {
@@ -125,91 +112,83 @@ function generateCars() {
 	}
 }
 
+function makeCarsDraggableWithClientOnly() {
+	$(".car").each(function () {
+		const $car = $(this);
+		const $client = $car.find(".client");
+
+		if ($client.length > 0) {
+			$car.draggable({
+				revert: "invalid",
+				helper: "clone",
+				containment: "body"
+			});
+		} else {
+			if ($car.hasClass("ui-draggable")) {
+				$car.draggable("destroy");
+			}
+		}
+	});
+}
+
 function setupClientDropzones() {
 	$(".client-drop").droppable({
-		accept: function(draggable) {
+		accept: function (draggable) {
 			const clientPreference = draggable.data("preference");
 			const dropBrand = $(this).data("brand");
-			return clientPreference === dropBrand;
+
+			if (clientPreference === dropBrand) return true;
+
+			const totalCars = brands[clientPreference]?.count || 0;
+			const emptySlots = $(`.client-drop[data-brand='${clientPreference}']`).filter(function () {
+				return $(this).children(".client").length === 0;
+			}).length;
+
+			if (emptySlots === 0 || totalCars === 0) {
+				return true;
+			}
+			return false;
 		},
-		over: function(event, ui) {
+		over: function () {
 			$(this).addClass("highlight-dropzone");
 		},
-		out: function(event, ui) {
+		out: function () {
 			$(this).removeClass("highlight-dropzone");
 		},
-		drop: function(event, ui) {
+		drop: function (event, ui) {
 			$(this).removeClass("highlight-dropzone");
 
 			const $dropzone = $(this);
 			const dropBrand = $dropzone.data("brand");
 			const clientId = ui.draggable.data("client-id");
 
-			// Check dari queue ke tak
 			const fromQueue = $(`#clients_queue .client[data-client-id='${clientId}']`).length > 0;
 
-			// Kalau dropzone dah ada client lain
-			const $existingClient = $dropzone.children(".client");
+			let $client = fromQueue
+				? $(`#clients_queue .client[data-client-id='${clientId}']`)
+				: ui.draggable;
 
-			if ($existingClient.length > 0) {
-				if (fromQueue) {
-					// Client dari queue tak boleh drop kalau tempat dah ada orang
-					ui.draggable.draggable("option", "revert", true);
-					return;
-				}
-
-				// === SWAP ===
-
-				// Cari dropzone asal client yang sedang ditarik
-				const $sourceDropzone = ui.draggable.closest(".client-drop");
-
-				// Swap antara dua dropzone
-				$sourceDropzone.empty().append($existingClient);
-				$dropzone.empty().append(ui.draggable);
-
-				// Reset gaya dua-dua client
-				[$existingClient, ui.draggable].forEach($client => {
-					$client
-						.addClass("draggable")
-						.css({
-							position: "static",
-							width: "100%",
-							height: "100%",
-							backgroundSize: "cover",
-							backgroundPosition: "center"
-						})
-						.find("span").css({
-							fontSize: "0.8rem",
-							color: "#fff",
-							textAlign: "center"
-						});
-
-					$client.draggable({
-						revert: "invalid",
-						helper: "clone",
-						containment: "body"
-					});
-				});
-
-				return;
+			// Remove dari queue jika ada
+			if (fromQueue) {
+				$client.remove();
+				$("#client_in_queue").html($("#clients_queue").children().length);
+				updateDraggableQueue();
 			}
 
-			// === NORMAL DROP (tempat kosong) ===
+			// Remove mana-mana client lain dalam dropzone
+			$dropzone.find(".client").remove();
 
-			// Ambil dari queue atau kereta
-			let $client = $(`#clients_queue .client[data-client-id='${clientId}']`);
-			if ($client.length === 0) {
-				$client = ui.draggable;
-			}
+			// Remove mana-mana duplicate client dari tempat lain
+			$(`.client-drop .client[data-client-id='${clientId}']`).each(function () {
+				const $oldDropzone = $(this).closest('.client-drop');
+				$(this).remove();
+				$oldDropzone.html("Drop client here");
+			});
 
-			// Kosongkan dropzone lama kalau bukan dari queue
-			const $oldDropzone = $client.closest(".client-drop");
-			if (!fromQueue && !$oldDropzone.is($dropzone)) {
-				$oldDropzone.empty().text("Drop client here");
-			}
-
+			// Buang span preference
 			$client.find(".preference").remove();
 
+			// Styling & draggable semula
 			$client
 				.addClass("draggable")
 				.css({
@@ -225,36 +204,76 @@ function setupClientDropzones() {
 					textAlign: "center"
 				});
 
-			$dropzone.empty().append($client);
-
 			$client.draggable({
 				revert: "invalid",
 				helper: "clone",
 				containment: "body"
 			});
 
-			// Kalau dari queue, buang dari queue
-			if (fromQueue) {
-				// Cari semula client asal dalam queue dan buang dia dari situ
-				const $queuedClient = $(`#clients_queue .client[data-client-id='${clientId}']`);
-				$queuedClient.remove();
+			// Letak dalam dropzone
+			$dropzone.empty().append($client);
 
-				$("#client_in_queue").html($("#clients_queue").children().length);
-				updateDraggableQueue();
-			}
+			makeCarsDraggableWithClientOnly();
 		}
 	});
 }
 
-$("document").ready(function(e) {
-	let clientServed, carsSold, amount;
+function cashier() {
+	$("#cashier").droppable({
+		accept: ".car",
+		over: function () {
+			$(this).addClass("highlight-dropzone");
+		},
+		out: function () {
+			$(this).removeClass("highlight-dropzone");
+		},
+		drop: function (event, ui) {
+			$(this).removeClass("highlight-dropzone");
 
-	$("#clients_served").html(clientServed);
-	$("#cars_sold").html(carsSold);
-	$("#amount").html(amount);
+			const $car = ui.draggable;
+			const $client = $car.find(".client");
+
+			if ($client.length === 0) return;
+
+			const $dropzone = $car.find(".client-drop");
+			const brand = $dropzone.data("brand");
+			const brandLower = brand.toLowerCase();
+
+			if ($dropzone.length) {
+				$dropzone.empty().html("Drop client here");
+			}
+
+			$car.remove();
+
+			brands[brand].count--;
+			$(`#stock_${brandLower}`).text(brands[brand].count);
+
+			let clientsServed = parseInt($("#clients_served").text()) || 0;
+			let carsSold = parseInt($("#cars_sold").text()) || 0;
+			let amount = parseFloat($("#amount").text().replace(/,/g, "")) || 0;
+
+			clientsServed++;
+			carsSold++;
+			amount += brands[brand].price;
+
+			$("#clients_served").text(clientsServed);
+			$("#cars_sold").text(carsSold);
+			$("#amount").text(amount.toLocaleString("en-MY", {
+				minimumFractionDigits: 2,
+				maximumFractionDigits: 2
+			}));
+		}
+	});
+}
+
+$(document).ready(function () {
+	$("#clients_served").html("0");
+	$("#cars_sold").html("0");
+	$("#amount").html("0.00");
 
 	newClient();
 	exit();
 	generateCars();
 	setupClientDropzones();
+	cashier();
 });
